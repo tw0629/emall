6caaffa66155a9bbed95260e72abf3c65884bc14
chiahaolu <lujiahao0708@gmail.com>
2017-11-13 15:02:52 +0800

    update

--- emall-sso/src/main/java/com/lujiahao/sso/filter/HeadersCORSFilter.java
+++ emall-sso/src/main/java/com/lujiahao/sso/filter/HeadersCORSFilter.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
+ * 跨域Filter
  * @author lujiahao
  * @version 1.0
  * @date 2017-10-15 22:20
@@ -19,10 +20,10 @@ public class HeadersCORSFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin","*");
-//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-//        response.setHeader("Access-Control-Max-Age", "3600");
-//        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");
-//        response.setHeader("Access-Control-Allow-Credentials","true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,content-type");
+        response.setHeader("Access-Control-Allow-Credentials","true");
         filterChain.doFilter(servletRequest, response);
     }
 
